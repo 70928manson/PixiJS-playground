d2e5cea7791ab14c86fce7b74f6f0157598d2a34
Manson <manson972@yahoo.com>
2024-02-28 16:12:24 +0800

    feat: pixijs Hello world

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
-import { Application } from 'pixi.js';
+import { Application, Text } from 'pixi.js';
 import './style.css';
 
 //遊戲程式進入點
 let app = new Application<HTMLCanvasElement>();
 document.body.appendChild(app.view);
+
+let text = new Text('Hello world', {
+    fill: 0x00FF00,
+});
+app.stage.addChild(text);
