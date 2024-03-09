e37756d075770f6ce6e8f78ae40f026cab266b02
Manson <manson972@yahoo.com>
2024-03-09 18:17:53 +0800

    feat: implement container

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
-import { Application, Text } from 'pixi.js';
+import { Application, Container, Text } from 'pixi.js';
 import './style.css';
 
 //遊戲程式進入點
 let app = new Application<HTMLCanvasElement>();
 document.body.appendChild(app.view);
 
-let text = new Text('Hello world', {
+let container = new Container();
+app.stage.addChild(container);
+
+let text1 = new Text('Hello world', {
     fill: 0x00FF00,
 });
-app.stage.addChild(text);
+let text2 = new Text('哈囉世界', {
+    fill: 0x00FF00,
+});
+container.addChild(text1);
+container.addChild(text2);
+
+text1.position.set(100, 50);
+text2.position.set(100, 100);
+container.angle = 15;
