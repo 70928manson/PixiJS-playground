e2eb7d00e8edb424519d62983a8c2f3ca96f3e36
Manson <manson972@yahoo.com>
2024-03-23 19:01:09 +0800

    feat: add line in app stage

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,30 @@
-import { Application, Container, Text, Sprite } from 'pixi.js';
+import { Application, Container, Text, Sprite, Graphics } from 'pixi.js';
 import './style.css';
 import testImageUrl from './images/test.jpg';
 
-//遊戲程式進入點
+// 遊戲程式進入點
 let app = new Application<HTMLCanvasElement>();
 document.body.appendChild(app.view);
 
-let sprite = Sprite.from(testImageUrl);
-app.stage.addChild(sprite);
+let graphics = new Graphics();
+app.stage.addChild(graphics);
+
+// 設定畫線的筆刷 (粗 20px 白色)
+graphics.lineStyle({
+    width: 20,
+    color: 0xFFFFFF,
+});
+// 把筆移動到(40, 50)
+graphics.moveTo(40, 50);
+// 畫線畫到(140, 30)
+graphics.lineTo(140, 30);
+// 提起筆再移動到(200, 30)
+graphics.moveTo(200, 30);
+// 下筆畫到(300, 50)
+graphics.lineTo(300, 50);
+
+// let sprite = Sprite.from(testImageUrl);
+// app.stage.addChild(sprite);
 
 let container = new Container();
 app.stage.addChild(container);
