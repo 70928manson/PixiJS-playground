1527947515e23daffa883414749889436f196aa4
Manson <manson972@yahoo.com>
2024-05-25 19:54:36 +0800

    feat: 功能 - 若array裡沒有item, 將item加進array

--- src/lib/ArrayUtils.ts
+++ src/lib/ArrayUtils.ts
@@ -1,3 +1,18 @@
 export class ArrayUtils {
-    ;
+    /**
+     * 在陣列裡加入唯一元素
+     * @param array 目標陣列
+     * @param item 要加入的元素
+     * @returns 若元素是唯一的且被加入則回傳true
+     */
+    static addUniqueItem (
+        array: unknown[],
+        item: unknown
+    ): boolean {
+        if (array.includes(item)) {
+            return false
+        }
+        array.push(item);
+        return true;
+    }
 }
