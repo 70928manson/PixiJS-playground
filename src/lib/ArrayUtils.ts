6096ffab586986bf8bbf911d762a4d93a7234eb1
Manson <manson972@yahoo.com>
2024-06-01 20:21:23 +0800

    feat: 新增 removeItem array utils

--- src/lib/ArrayUtils.ts
+++ src/lib/ArrayUtils.ts
@@ -15,4 +15,23 @@ export class ArrayUtils {
         array.push(item);
         return true;
     }
+
+    /**
+     * 移除陣列中的一個元素
+     * @param array 目標陣列
+     * @param item  要移除的元素
+     * @returns 若成功移除元素則回傳true
+     */
+    static removeItem(
+        array: unknown[],
+        item: unknown
+    ): boolean {
+        let index = array.indexOf(item);
+        if (index === -1) {
+            return false
+        };
+        // 使用splice()在 index 的位置刪除1個元素
+        array.splice(index, 1);
+        return true;
+    }
 }
