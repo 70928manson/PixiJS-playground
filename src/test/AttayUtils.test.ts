1ba713e2abba8230a305ab4d2413b7b8d38d8c45
Manson <manson972@yahoo.com>
2024-07-27 12:59:41 +0800

    feat: 新增 sortNumericOn test

--- src/test/AttayUtils.test.ts
+++ src/test/AttayUtils.test.ts
@@ -8,3 +8,15 @@ test('addUniqueItem', () => {
     expect(ArrayUtils.addUniqueItem(array, "d")).toBe(true);
     expect(array.length).toBe(4);
 });
+
+test('物件陣列: 依屬性排列', () => {
+    let array = [
+        { id: 'p1', name: 'Karsa', power: 10 },
+        { id: 'p2', name: 'Maple', power: 23 },
+        { id: 'p3', name: 'Westdoor', power: 99 }
+    ];
+    ArrayUtils.sortNumericOn(array, 'power', true);
+    
+    // 預期由大到小排序, 第一個元素power為99
+    expect(array[0].power).toBe(99);
+});
