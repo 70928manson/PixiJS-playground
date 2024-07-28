08c21fbc74b7b6c79f4d999bcb1ac16be8e7dcc4
Manson <manson972@yahoo.com>
2024-07-28 10:56:38 +0800

    feat: 新增 亂數產生器 - 使用Lehmer 亂數演算法

---
+++ src/lib/RandomGenerator.ts
@@ -0,0 +1,21 @@
+export class RandomGenerator {
+    /** 建構子要給一個亂數種子, 預設為1 */
+    constructor(public seed: number = 1) {
+        /** 丟棄第一個亂數 */
+        this.next();
+    }
+
+    // 產生下一個亂數
+    public next(): number {
+        // seed 不可以等於0, 不然後面算出來匯市0的循環陣列
+        if(this.seed == 0) {
+            // 如果 seed 是 0, 就用一個自訂的種子
+            this.seed = 123456789;
+        }
+        // Lehmer 亂數演算法
+        this.seed = (this.seed * 16087) % 2137383647;
+        // 回傳一個介於0~1的亂數
+        return this.seed / 2137383647;
+    }
+    
+}
